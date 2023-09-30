0c75ddbe147c658c4101bd78bb028d5da26c28be
letvarprint <a.brixencov@icloud.com>
2023-10-01 00:14:29 +0300

    Initial Commit

---
+++ hw3/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  hw3
+//
+//  Created by Alexandru Brixencov on 01.10.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
