ca390a9d337e9eb332391e89f21236c72bd6b010
letvarprint <a.brixencov@icloud.com>
2023-10-01 03:54:08 +0300

    Created everything

--- hw3/ViewController.swift
+++ hw3/ViewController.swift
@@ -9,11 +9,46 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var mainView: UIView!
+    
+    @IBOutlet weak var redCountLabel: UILabel!
+    @IBOutlet weak var greenCountLabel: UILabel!
+    @IBOutlet weak var blueCountLabel: UILabel!
+    
+    @IBOutlet weak var redSlider: UISlider!
+    @IBOutlet weak var blueSlider: UISlider!
+    @IBOutlet weak var greenSlider: UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        changeViewColor()
+        
+        redCountLabel.text = String(format: "%.2f", redSlider.value)
+        greenCountLabel.text = String(format: "%.2f", greenSlider.value)
+        blueCountLabel.text = String(format: "%.2f", blueSlider.value)
     }
 
-
+    @IBAction func redSliderAction(_ sender: UISlider) {
+        changeViewColor()
+    
+        switch sender {
+        case redSlider:
+            redCountLabel.text = String(format: "%.2f", redSlider.value)
+        case greenSlider:
+            greenCountLabel.text = String(format: "%.2f", greenSlider.value)
+        case blueSlider:
+            blueCountLabel.text = String(format: "%.2f", blueSlider.value)
+        default:
+            break
+        }
+        
+    }
+    
+    private func changeViewColor() {
+        mainView.backgroundColor = UIColor(red: CGFloat(redSlider.value), 
+                                           green: CGFloat(greenSlider.value),
+                                           blue: CGFloat(blueSlider.value), 
+                                           alpha: 1)
+    }
 }
 
